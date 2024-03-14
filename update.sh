f426524bfa5654ae9c061ecf103e281afa60a099
Leon <81116130+leonb033@users.noreply.github.com>
2024-03-14 20:51:58 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,20 @@
+set -x
+
 # clean apt cache
 apt clean -y
+echo
+
 # search for package updates
 apt update
+echo
+
 # install available package updates
 apt upgrade -y
+echo
+
 # delete unused dependency packages
 apt autoremove --purge -y
+echo
+
 # reboot
 systemctl reboot
